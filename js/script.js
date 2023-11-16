878b7135b56729fd056c16da5443ef833d358b31
elboyko <boyko.lena@gmail.com>
2023-11-16 11:47:32 +0700

    add mobile window for adaptive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('hello');
+const menuBtn = document.querySelector('.menu__btn');
+const menuClose = document.querySelector('.menu__close');
+const menuList = document.querySelector('.menu__list');
+const menuTiny = document.querySelector('.menu--close');
+
+menuBtn.addEventListener('click', () => {
+   menuList.classList.toggle('menu__list--open');
+   menuTiny.classList.toggle('menu--open');//тень для заднего фона при мобильном меню
+
+})
+menuClose.addEventListener('click', () => {
+   menuList.classList.remove('menu__list--open');
+   menuTiny.classList.remove('menu--open');
+
+})
